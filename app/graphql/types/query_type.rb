19f3f386118555fa1cb421ef8db231079214330c
shawneegao <shawneegao@gmail.com>
2019-03-02 21:52:04 -0800

    step 1 of metaprogramming

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,12 +10,15 @@ module Types
 
     # this class method creates a field instance from a list of arguments, keyword arguments, and a block
 
+    # Instead of defining it here explicitly it would be dope to just lookup
+    # the constants in the Types module
     VALID_ROOT_TYPES = [Types::PaymentType, Types::MerchantType]
 
     for type in VALID_ROOT_TYPES do
       # use introspection lookup gql type - error if none 
-      # get the constant! 
-      #unless Types.const_defined?(type) raise "Type #{type} is not not a valid entry point into the schema"
+
+      # add some saftey checks
+      # unless Types.const_defined?(type) raise "Type #{type} is not not a valid entry point into the schema"
 
       fieldName = /Types::(.*?)Type/.match(type.to_s)[1].downcase
       field fieldName, type, null: false do
@@ -23,20 +26,45 @@ module Types
       end
     end
 
-    # this is what we want to metaprogram away 
+    # Old code that we metaprogrammed away! 
+
     # field :payment, Types::PaymentType, null: false do
     #   binding.pry
     #   argument :id, ID, required: true
     # end
 
-    # demo how this is not working but graphiql shows you example how
-    # gql ruby works and its process 
-    # this is an instance method
+    # def payment(params)
+    #   Payment.lookup(params[:id])
+    # end
 
     def payment(params)
-      Payment.lookup(params[:id])
+      testLookup :payment, params
     end
 
-    
+    def testLookup(fieldName, params)
+      targetClass = Object.const_get(fieldName.capitalize)
+      targetClass.lookup(params[:id])
+    end
+
+    # The next step: 
+    # define_method :some_method do |params|
+    #   if yield ? yield : 'no custom block! going to default field lookup!'
+    #     Payment.lookup(params[:id])
+    #   #check if there is a block, if there is a block, then run that
+    #   # other wise do a target lookup by id
+    # end
+
+    # Comments in my head:
+    # use block_given? to check for custom lookup 
+
+    # Have some more sophisticated const checks
+    # def class_from_string(str)
+    #   str.split('::').inject(Object) do |mod, class_name|
+    #     mod.const_get(class_name)
+    #   end
+    # end
+
+    #lol why is Object.const_defined?(Payment.to_s) true
+    # Object.const_defined?("Payment") false?
   end
 end
